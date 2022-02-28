8729680b4075f80348a0c868ce8f85f49ed2c704
Peter Close <pclose99@gmail.com>
2022-02-28 16:08:43 -0500

    plz

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // Require Express.js
 // Start app
-const express = require('express')
+import minimist from 'minimist';
+import express from 'express'
 const app = express()
 
 // Get port from terminal 
-const args = require('minimist')(process.argv.slice(2));
+
+const args = minimist(process.argv.slice(2));
 args['port']
 var port = args.port || 5000
 
@@ -71,24 +73,22 @@ function flipACoin(call) {
     }
     return { call: call, flip: res, result: result }
 }
+
+// Default response for any other request
+app.use(function(req, res){
+  res.status(404).send('404 NOT FOUND')
+  res.type("text/plain")
+})
   
 // ------------------------------------------------------------------------------
 // -------------------------------- ENDPOINTS ----------------------------------
 // ------------------------------------------------------------------------------
 
-// Default response for any other request
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
-    res.type("text/plain")
-})
+
 
 app.get('/app/', (req, res) => {
-    // Respond with status 200
-    res.statusCode = 200;
-    // Respond with status message "OK"
-    res.statusMessage = 'OK';
-    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-    res.end(res.statusCode+ ' ' +res.statusMessage)
+    res.status(200).end('OK')
+    res.type("text/plain")
 })
 
 app.get('/app/flip/', (req, res) => {
@@ -102,12 +102,12 @@ app.get('/app/flips/:number/', (req, res) => {
 })
 
 // endpoints for heads and tails
-app.get('/app/flips/heads/', (req, res) => {
+app.get('/app/flips/call/heads/', (req, res) => {
     var temp = flipACoin('heads')
     res.status(200).json(temp)
 })
 
-app.get('/app/flips/tails/', (req, res) => {
+app.get('/app/flips/call/tails/', (req, res) => {
   var temp = flipACoin('tails')
   res.status(200).json(temp)
 })
