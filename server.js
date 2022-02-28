5059c66663966123c6812310a0cbf6d7e9004452
Peter Close <pclose99@gmail.com>
2022-02-28 15:22:48 -0500

    added coin functions

--- server.js
+++ server.js
@@ -8,11 +8,73 @@ const args = require('minimist')(process.argv.slice(2));
 args['port']
 var port = args.port || 5000
 
+// Use express
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Log request body and reponse body
+const logging = (req, res, next) => {
+    console.log(req.body.number)
+    next()
+}
+
 // Start an app server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
 });
 
+// ------------------------------------------------------------------------------
+// -------------------------------- COIN STUFF ----------------------------------
+// ------------------------------------------------------------------------------
+function coinFlip() {
+    return Math.random() < 0.5 ? ('heads') : ('tails')
+}
+
+function coinFlips(flips) {
+    const arr = []
+    for(let i = 0; i<flips; i++) {
+      arr[i] = coinFlip()
+    }
+    return arr
+}
+
+
+function countFlips(flips) {
+    let hCnt = 0
+    let tCnt = 0
+    for(let i=0; i<flips.length; i++) {
+      if(flips[i] == 'heads') {
+        hCnt++;
+      } else {
+        tCnt++;
+      }
+    }
+  
+    if(hCnt == 0) {
+      return {tails: tCnt}
+    }
+    if(tCnt == 0) {
+      return {heads: hCnt}
+    }
+    return { heads: hCnt, tails: tCnt }
+}
+
+
+function flipACoin(call) {
+    let res = coinFlip()
+    var result;
+    if(call == res) {
+      
+      result = 'win'
+    } else {
+      result = 'lose'
+    }
+    return { call: call, flip: res, result: result }
+}
+  
+// ------------------------------------------------------------------------------
+// -------------------------------- ENDPOINTS ----------------------------------
+// ------------------------------------------------------------------------------
 
 // Default response for any other request
 app.use(function(req, res){
